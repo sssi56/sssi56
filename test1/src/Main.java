a2cc1b2a3af81457ec798a0ae72a84eae517227e
debian <sidorov30@yandex.ru>
2025-01-30 14:25:01 +0500

    add all

---
+++ test1/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        
+    }
+}
