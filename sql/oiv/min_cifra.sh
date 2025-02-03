3b79cd673518c56617d605022ecf48bbc521723e
debian <sidorov30@yandex.ru>
2025-02-03 17:06:28 +0500

    update

--- sql/oiv/min_cifra.sh
+++ sql/oiv/min_cifra.sh
@@ -1,7 +1,3 @@
-psql -h 172.18.0.2 -U postgres -p 5432 -d cm6 -c "DROP SCHEMA public CASCADE;"
-psql -h 172.18.0.2 -U postgres -p 5432 -d cm6 -c "VACUUM FULL VERBOSE ANALYZE;"
-psql -h 172.18.0.2 -U postgres -p 5432 -d cm6 -c "CREATE SCHEMA public;"
-
 pg_dump -h 172.28.123.16 -U postgres -d cm6 \
  -t so_department \
  -t department \
